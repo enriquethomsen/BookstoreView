f9757f9109ab33d92f65ce182821cee4c380f8ff
Enriqué T <167785063+enriquethomsen@users.noreply.github.com>
2024-05-02 17:25:36 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,26 @@
 import express from 'express';
+/* import { PORT, mongoDBURL } from './config.js'; */
 import mongoose from 'mongoose';
 import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
 
 const app = express();
 const MONGO_URL = process.env.MONGO_URL || MONGO_URL
+
+
 // Middleware for parsing request body
 app.use(express.json());
 
 // Middleware for handling CORS POLICY
-// Option 1: Allow All Origins with Default of cors(*)
-app.use(cors());
-// Option 2: Allow Custom Origins
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-//   })
-// );
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json({ data: "hello" });
+});
 
 app.get('/', (request, response) => {
   console.log(request);
@@ -29,6 +31,12 @@ app.use('/books', booksRoute);
 
 mongoose
   .connect(MONGO_URL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: `);
+    });
+  })
   .catch((error) => {
     console.log(error);
   });
